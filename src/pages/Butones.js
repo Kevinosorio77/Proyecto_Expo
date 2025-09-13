e82dc06b22494b053b43281ce3d7ba7709235074
andrew <andrewosorio589@gmail.com>
2025-09-12 22:01:23 -0600

    Botones 3

--- src/pages/Butones.js
+++ src/pages/Butones.js
@@ -1,21 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text,StyleSheet, Platform, StatusBar} from "react-native";
+import { View, Text, StyleSheet, Platform, StatusBar } from "react-native";
 
-export default function Botones(){
-return(
-<View style={style.mainS}>
-
-    <Text>Botones</Text>
-</View>
-
-);
-
+export default function Botones() {
+  return (
+    <View style={styles.mainS}>
+      <Text>Botones</Text>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-mainS:{
-    flex:1,
-    backgroundColor: '#2e98c5ff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
-    padding:16
-}
-})
+  mainS: {
+    flex: 1,
+    backgroundColor: "#2e98c5ff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 44,
+    padding: 16,
+  },
+});
